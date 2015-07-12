bbe9f677d976d34dc7f36e8fb7690ec612e724ef
Eddie Ferrer <eferrer@gmail.com>
2015-07-11 20:46:33 -0700

    Added sorting, added off canvas form

--- js/main.js
+++ js/main.js
@@ -43,8 +43,15 @@ $(function() {
           record.lastUpdate = $li.find('td:nth-child(13)').html();
     }
 
-
     var dynatable = $('#mazama_climb_load')
+    .bind('dynatable:init', function(e, dynatable) {
+      $('#sort').change( function(e) {
+        dynatable.sorts.clear();
+        dynatable.sorts.add($( "#sort option:selected" ).attr("data-value"), $( "#sort-direction" ).attr("data-direction")) // 1=ASCENDING, -1=DESCENDING
+        dynatable.process();
+        e.preventDefault();
+      });
+    })
     .bind('dynatable:init', function(e, dynatable) {
       dynatable.queries.functions["search-grade"] = function(record, queryValue) {
         for (var i = 0; i < queryValue.length; i++) {
@@ -94,6 +101,19 @@ $(function() {
           dynatable.queries.remove("search-grade");
         }
         dynatable.process();
+    });
+    $('#sort-direction').click( function(e) {
+      e.preventDefault();
+      if ( $(this).attr("data-direction") === "-1") {
+        $(this).attr("data-direction", "1");
+        $('#sort').trigger("change");
+        return
+      }
+      if ( $(this).attr("data-direction") === "1") {
+        $(this).attr("data-direction", "-1");
+        $('#sort').trigger("change");
+        return
+      }
     });
 
 });
